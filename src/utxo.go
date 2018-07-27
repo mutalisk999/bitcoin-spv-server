0bf0c69073ade220bff572d2e1b4d11bd5580d3f
mutalisk <0x08@0x08.net>
2018-07-27 15:46:45 +0800

    add data structure about utxo

---
+++ src/utxo.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"github.com/mutalisk999/bitcoin-lib/src/bigint"
+	"github.com/mutalisk999/bitcoin-lib/src/blob"
+	"github.com/mutalisk999/bitcoin-lib/src/script"
+	"github.com/mutalisk999/bitcoin-lib/src/serialize"
+	"io"
+)
+
+type UtxoSource struct {
+	TrxId bigint.Uint256
+	Vout  int32
+}
+
+func (u UtxoSource) Pack(writer io.Writer) error {
+	err := u.TrxId.Pack(writer)
+	if err != nil {
+		return err
+	}
+	err = serialize.PackInt32(writer, u.Vout)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UtxoSource) UnPack(reader io.Reader) error {
+	err := u.TrxId.UnPack(reader)
+	if err != nil {
+		return err
+	}
+	u.Vout, err = serialize.UnPackInt32(reader)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type UtxoDetail struct {
+	Amount       int64
+	Address      string
+	ScriptPubKey script.Script
+	Status       byte
+}
+
+func (u UtxoDetail) Pack(writer io.Writer) error {
+	err := serialize.PackInt64(writer, u.Amount)
+	if err != nil {
+		return err
+	}
+	var bytesAddr blob.Byteblob
+	bytesAddr.SetData([]byte(u.Address))
+	err = bytesAddr.Pack(writer)
+	if err != nil {
+		return err
+	}
+	err = u.ScriptPubKey.Pack(writer)
+	if err != nil {
+		return err
+	}
+	err = serialize.PackByte(writer, u.Status)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UtxoDetail) UnPack(reader io.Reader) error {
+	var err error
+	u.Amount, err = serialize.UnPackInt64(reader)
+	if err != nil {
+		return err
+	}
+	var bytesAddr blob.Byteblob
+	err = bytesAddr.UnPack(reader)
+	if err != nil {
+		return err
+	}
+	u.Address = string(bytesAddr.GetData())
+	err = u.ScriptPubKey.UnPack(reader)
+	if err != nil {
+		return err
+	}
+	u.Status, err = serialize.UnPackByte(reader)
+	if err != nil {
+		return err
+	}
+	return nil
+}
